64bb3a8dad488c918b8015e15997d5a07bb66f49
leohsieh <bardiche4768@gmail.com>
2023-12-12 15:17:54 -0500

    modulated dataset and lora

---
+++ dataset.py
@@ -0,0 +1,94 @@
+import glob
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+
+from PIL import Image
+
+from pycocotools import mask as mask_utils
+
+import json
+import numpy as np
+from tqdm import tqdm
+
+
+input_transforms = transforms.Compose([
+    transforms.Resize((160, 256)),
+    transforms.ToTensor(),
+])
+
+target_transforms = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Resize((160, 256)),
+])
+
+class SA1B_Dataset(torchvision.datasets.ImageFolder):
+    """A data loader for the SA-1B Dataset from "Segment Anything" (SAM)
+    This class inherits from :class:`~torchvision.datasets.ImageFolder` so
+    the same methods can be overridden to customize the dataset.
+    Args:
+        root (string): Root directory path.
+        transform (callable, optional): A function/transform that  takes in an PIL image
+            and returns a transformed version. E.g, ``transforms.RandomCrop``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+        loader (callable, optional): A function to load an image given its path.
+        is_valid_file (callable, optional): A function that takes path of an Image file
+            and check if the file is a valid file (used to check of corrupt files)
+     Attributes:
+        classes (list): List of the class names sorted alphabetically.
+        class_to_idx (dict): Dict with items (class_name, class_index).
+        imgs (list): List of (image path, class_index) tuples
+    """
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+        Returns:
+            tuple: (sample, target) where target is class_index of the target class.
+        """
+        path, _ = self.imgs[index] # discard automatic subfolder labels
+        image = self.loader(path)
+        masks = json.load(open(f'{path[:-3]}json'))['annotations'] # load json masks
+        target = []
+
+        for m in masks:
+            # decode masks from COCO RLE format
+            target.append(mask_utils.decode(m['segmentation']))
+        target = np.stack(target, axis=-1)
+
+        if self.transform is not None:
+            image = self.transform(image)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+        target[target > 0] = 1 # convert to binary masks
+
+        return image, target
+
+    def __len__(self):
+        return len(self.imgs)
+
+
+input_reverse_transforms = transforms.Compose([
+    transforms.ToPILImage(),
+])
+
+import matplotlib.pyplot as plt
+def show_image(image, target, row=12, col=12):
+    # image: numpy image
+    # target: mask [N, H, W]
+    fig, axs = plt.subplots(row, col, figsize=(20, 12))
+    for i in range(row):
+        for j in range(col):
+            if i*row+j < target.shape[0]:
+                axs[i, j].imshow(image)
+                axs[i, j].imshow(target[i*row+j], alpha=0.5)
+            else:
+                axs[i, j].imshow(image)
+            axs[i, j].axis('off')
+    plt.tight_layout()
+    plt.show()
---
+++ lora.py
@@ -0,0 +1,243 @@
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+
+
+def apply_lora(parent_block: nn.Module, device):
+    for name, block in parent_block.named_children():
+        if isinstance(block, nn.Linear):
+            block = MonkeyPatchLoRALinear(block, 4, 1).to(device)
+            setattr(parent_block, name, block)
+
+        elif isinstance(block, nn.Conv2d):
+            block = MonkeyPatchLoRAConv2D(block, 4, 1).to(device)
+            setattr(parent_block, name, block)
+
+        elif isinstance(block, nn.ConvTranspose2d):
+            block = MonkeyPatchLoRAConvTranspose2D(block, 4, 1).to(device)
+            setattr(parent_block, name, block)
+
+        elif isinstance(block, nn.Module):
+            apply_lora(block, device)
+
+
+class LoRALinearLayer(nn.Module):
+    def __init__(self, in_features, out_features, rank=4):
+        super().__init__()
+
+        if rank > min(in_features, out_features):
+            raise ValueError(
+                f"LoRA rank {rank} must be less or equal than {min(in_features, out_features)}"
+            )
+
+        self.down = nn.Linear(in_features, rank, bias=False)
+        self.up = nn.Linear(rank, out_features, bias=False)
+
+        nn.init.normal_(self.down.weight, std=1 / rank)
+        nn.init.zeros_(self.up.weight)
+
+    def forward(self, hidden_states):
+        orig_dtype = hidden_states.dtype
+        dtype = self.down.weight.dtype
+
+        down_hidden_states = self.down(hidden_states.to(dtype))
+        up_hidden_states = self.up(down_hidden_states)
+
+        return up_hidden_states.to(orig_dtype)
+
+    @property
+    def weight(self):
+        return self.up.weight @ self.down.weight
+
+    @property
+    def bias(self):
+        return 0
+
+class MonkeyPatchLoRALinear(nn.Module):
+    # It's "monkey patch" means you can replace nn.Linear with the new
+    # LoRA Linear class without modifying any other code.
+    def __init__(self, fc: nn.Linear, rank=4, lora_scale=1):
+        super().__init__()
+        if rank > min(fc.in_features, fc.out_features):
+            raise ValueError(
+                f"LoRA rank {rank} must be less or equal than {min(fc.in_features, fc.out_features)}"
+            )
+        if not isinstance(fc, nn.Linear):
+            raise ValueError(
+                f"MonkeyPatchLoRALinear only support nn.Linear, but got {type(fc)}"
+            )
+
+        self.fc = fc
+        self.rank = rank
+        self.lora_scale = lora_scale
+
+        in_features = fc.in_features
+        out_features = fc.out_features
+        self.fc_lora = LoRALinearLayer(in_features, out_features, rank)
+
+    def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
+        hidden_states = self.fc(hidden_states) + \
+                        self.lora_scale * self.fc_lora(hidden_states)
+        return hidden_states
+
+    @property
+    def weight(self):
+        return self.fc.weight + self.lora_scale * self.fc_lora.weight
+
+    @property
+    def bias(self):
+        return self.fc.bias
+
+# your implementation
+
+class LoRAConv2DLayer(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, rank=4):
+        super().__init__()
+        # if rank > min(in_channels * kernel_size**2, out_channels):
+        #     raise ValueError(
+        #         f"LoRA rank {rank} must be less or equal than {min(in_channels * kernel_size**2, out_channels)}"
+        #     )
+
+        self.down = nn.Conv2d(in_channels, rank, kernel_size, stride, padding, bias=False)
+        self.up = nn.Conv2d(rank, out_channels, 1, 1, 0, bias=False)
+
+        nn.init.normal_(self.down.weight, std=1 / rank)
+        nn.init.zeros_(self.up.weight)
+
+    def forward(self, hidden_states):
+        orig_dtype = hidden_states.dtype
+        dtype = self.down.weight.dtype
+
+        down_hidden_states = F.conv2d(hidden_states.to(dtype), self.down.weight, self.down.bias, self.down.stride, self.down.padding)
+        up_hidden_states = F.conv2d(down_hidden_states, self.up.weight, self.up.bias, self.up.stride, self.up.padding)
+
+        return up_hidden_states.to(orig_dtype)
+    
+    @property
+    def weight(self):
+        return self.up.weight @ self.down.weight
+
+    @property
+    def bias(self):
+        return 0
+
+class MonkeyPatchLoRAConv2D(nn.Module):
+    def __init__(self, conv2d: nn.Conv2d, rank=4, lora_scale=1):
+        super().__init__()
+        # if rank > min(conv2d.in_channels * conv2d.kernel_size[0]**2, conv2d.out_channels):
+        #     raise ValueError(
+        #         f"LoRA rank {rank} must be less or equal than {min(conv2d.in_channels * conv2d.kernel_size[0]**2, conv2d.out_channels)}"
+        #     )
+        if not isinstance(conv2d, nn.Conv2d):
+            raise ValueError(
+                f"MonkeyPatchLoRAConv2D only supports nn.Conv2d, but got {type(conv2d)}"
+            )
+
+        self.conv2d = conv2d
+        self.rank = rank
+        self.lora_scale = lora_scale
+
+        in_channels = conv2d.in_channels
+        out_channels = conv2d.out_channels
+        kernel_size = conv2d.kernel_size
+        stride = conv2d.stride
+        padding = conv2d.padding
+        self.conv2d_lora = LoRAConv2DLayer(in_channels, out_channels, kernel_size, stride, padding, rank)
+
+    def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
+        hidden_states = self.conv2d(hidden_states) + \
+                        self.lora_scale * self.conv2d_lora(hidden_states)
+        return hidden_states
+
+    @property
+    def weight(self):
+        return self.conv2d.weight + self.lora_scale * self.conv2d_lora.weight
+
+    @property
+    def bias(self):
+        return self.conv2d.bias
+
+class LoRAConvTranspose2DLayer(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, output_padding=0, rank=4):
+        super().__init__()
+
+        if rank > min(in_channels, out_channels * kernel_size**2):
+            raise ValueError(
+                f"LoRA rank {rank} must be less or equal than {min(in_channels, out_channels * kernel_size**2)}"
+            )
+
+        # Define the downsample convolution layer
+        self.down = nn.ConvTranspose2d(in_channels, rank, kernel_size, stride, padding, output_padding, bias=False)
+        
+        # Define the upsample convolution layer
+        self.up = nn.ConvTranspose2d(rank, out_channels, 1, 1, 0, bias=False)
+
+        # Initialize weights
+        nn.init.normal_(self.down.weight, std=1 / rank)
+        nn.init.zeros_(self.up.weight)
+
+    def forward(self, hidden_states):
+        orig_dtype = hidden_states.dtype
+        dtype = self.down.weight.dtype
+
+        # Perform the downsample convolution
+        down_hidden_states = F.conv_transpose2d(hidden_states.to(dtype), self.down.weight, self.down.bias, self.down.stride, self.down.padding, self.down.output_padding)
+        
+        # Perform the upsample convolution
+        up_hidden_states = F.conv_transpose2d(down_hidden_states, self.up.weight, self.up.bias, self.up.stride, self.up.padding, self.up.output_padding)
+
+        return up_hidden_states.to(orig_dtype)
+
+    @property
+    def weight(self):
+        return self.up.weight @ self.down.weight
+
+    @property
+    def bias(self):
+        return 0
+
+
+class MonkeyPatchLoRAConvTranspose2D(nn.Module):
+    def __init__(self, conv_transpose2d: nn.ConvTranspose2d, rank=4, lora_scale=1):
+        super().__init__()
+        if rank > min(conv_transpose2d.in_channels, conv_transpose2d.out_channels * conv_transpose2d.kernel_size[0]**2):
+            raise ValueError(
+                f"LoRA rank {rank} must be less or equal than {min(conv_transpose2d.in_channels, conv_transpose2d.out_channels * conv_transpose2d.kernel_size[0]**2)}"
+            )
+        if not isinstance(conv_transpose2d, nn.ConvTranspose2d):
+            raise ValueError(
+                f"MonkeyPatchLoRAConvTranspose2D only supports nn.ConvTranspose2d, but got {type(conv_transpose2d)}"
+            )
+
+        # Save the original ConvTranspose2D layer
+        self.conv_transpose2d = conv_transpose2d
+        self.rank = rank
+        self.lora_scale = lora_scale
+
+        # Create the LoRA ConvTranspose2D layer
+        in_channels = conv_transpose2d.in_channels
+        out_channels = conv_transpose2d.out_channels
+        kernel_size = conv_transpose2d.kernel_size
+        stride = conv_transpose2d.stride
+        padding = conv_transpose2d.padding
+        output_padding = conv_transpose2d.output_padding
+        self.conv_transpose2d_lora = LoRAConvTranspose2DLayer(in_channels, out_channels, kernel_size, stride, padding, output_padding, rank)
+
+    def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
+        # Forward pass using both the original ConvTranspose2D and LoRA ConvTranspose2D
+        hidden_states = self.conv_transpose2d(hidden_states) + \
+                        self.lora_scale * self.conv_transpose2d_lora(hidden_states)
+        return hidden_states
+
+    @property
+    def weight(self):
+        # Combine weights of the original ConvTranspose2D and LoRA ConvTranspose2D
+        return self.conv_transpose2d.weight + self.lora_scale * self.conv_transpose2d_lora.weight
+
+    @property
+    def bias(self):
+        # Return the bias of the original ConvTranspose2D
+        return self.conv_transpose2d.bias
