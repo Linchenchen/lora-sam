bf200112a5f81ca9e0721f1e33e7cae92e3bdfa2
leohsieh <bardiche4768@gmail.com>
2023-12-17 20:54:27 -0500

    used single __getitem__ in inference.py

--- inference.py
+++ inference.py
@@ -2,8 +2,21 @@ from lora_sam import *
 
 
 def inference(checkpoint = "lightning_logs/version_2/checkpoints/epoch=2-step=300.ckpt"):
+    input_transform = transforms.Compose([
+        transforms.Resize((160, 256), antialias=True),
+        transforms.ToTensor(),
+    ])
+
+    target_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Resize((160, 256), antialias=True),
+    ])
+
     plotter = Plotter(checkpoint)
-    train_loader, test_loader = get_loaders(batch_size=8)
+    dataset = SA1B_Dataset("./data", transform=input_transform, target_transform=target_transform)
+    #train_loader, test_loader = get_loaders(batch_size=8)
+
+    image, target = dataset.__getitem__(0)
 
     input_box = np.array([40, 50, 130, 110])
     input_point = np.array([[50,50], [75,75]])
@@ -12,10 +25,7 @@ def inference(checkpoint = "lightning_logs/version_2/checkpoints/epoch=2-step=30
     #kwargs["bboxes"] = input_box
     kwargs["coords"] = input_point
     kwargs["labels"] = input_label
-
-    
-    images, target = next(iter(test_loader))
-    plotter.inference_plot(images[0], target[0], **kwargs)
+    plotter.inference_plot(image, target, **kwargs)
 
 
 if __name__ == "__main__":
